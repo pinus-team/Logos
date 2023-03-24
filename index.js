e4231398cf1cf2b98a09a4b2211ac34fe5634fd8
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-03-25 00:52:54 +0700

    Convert to ES6

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require("express");
-const ejs = require("ejs");
-const bodyParser = require("body-parser");
-const path = require("path");
+import express from "express";
+import ejs from "ejs";
+import { join } from "path";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(
-	express.static(path.join(__dirname, "/node_modules/tw-elements/dist/js"))
-);
+import { endpoints } from "./routes/endpoints.js";
+
+app.use(express.static("public"));
+app.use(express.static("/node_modules/tw-elements/dist/js"));
+
+for (const endpoint of endpoints) {
+	app[endpoint.method](endpoint.path, endpoint.handler);
+}
 
 app.listen(3000, function () {
 	console.log("Server started on port 3000");

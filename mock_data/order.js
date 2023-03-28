f83f07981926af0a23ccb054efe61f966383fdb0
Hakushin <kpx1164@gmail.com>
2023-03-29 01:47:07 +0700

    Setup delivery frontend and add some mock order

--- mock_data/order.js
+++ mock_data/order.js
@@ -1,19 +1,32 @@
 export default [
-    {
-        _id: "763Db1",
-        timestamp: "2021-01-01",
-        name: "Pathi",
-        last_name: "Lumoh",
-        address:"235 CMU Maeung",
-
+  {
+    order_id: "763Db1",
+    order_date: "2021-01-01",
+    order_time: "12:00",
+    user_name: "Jane",
+    user_lastname: "Halo",
+    address: {
+      at: "Drom1",
+      house_number: "123/2",
+      city: "mueang",
+      post_code: "50200",
     },
-    {
-        _id: "763Db1",
-        timestamp: "2021-01-01",
-        name: "Nam",
-        last_name: "Meena",
-        address:"345 CNX SK",
-
-
+    payment: "Cash",
+    status: "Pickup",
+  },
+  {
+    order_id: "563Db2",
+    order_date: "2021-01-01",
+    order_time: "13:00",
+    user_name: "Tiw",
+    user_lastname: "Jak",
+    address: {
+      at: "Drom4",
+      house_number: "734/4",
+      city: "mueang",
+      post_code: "50100",
     },
-]
+    payment: "Credit",
+    status: "Pickup",
+  },
+];

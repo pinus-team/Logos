35f1842a74a4539ff1cfe520c4ac4310274295b8
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-03-31 05:17:07 +0700

    partial summary

--- routes/summary.js
+++ routes/summary.js
@@ -1,5 +1,29 @@
-import { getPrivateNameAndPath } from "./endpoints.js";
+import { getPrivateNameAndPath, host } from "./endpoints.js";
+import axios from "axios";
 
-export default (req, res) => {
-    res.render("summary", { title: "Pinus Sylvestris", endpoints: getPrivateNameAndPath(), user: req.user_data });
+export default async (req, res) => {
+	const sums = await axios
+		.get(`http://${host}:8000/order/summary/income`)
+		.then((result) => {
+			return result.data;
+		});
+	const best_selling_today = await axios
+		.get(`http://${host}:8000/order/summary/dish_today`)
+		.then((result) => {
+			return result.data;
+		});
+	const best_selling_all = await axios
+		.get(`http://${host}:8000/order/summary/dish_all`)
+		.then((result) => {
+			return result.data;
+		});
+
+	res.render("summary", {
+		title: "Pinus Sylvestris",
+		endpoints: getPrivateNameAndPath(),
+		user: req.user_data,
+		sums,
+		best_selling: best_selling_today.slice(0, 5),
+		best_selling_all: best_selling_all.slice(0, 5),
+	});
 };
